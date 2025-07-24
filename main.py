aab6e37493972a5c21aabcbc47c10b1cec20412d
Lazarokaua <lazarokaua4@gmail.com>
2025-07-24 17:01:20 -0300

    feat: adicionado lógica inicial para mover as images

--- main.py
+++ main.py
@@ -12,4 +12,21 @@ IMAGE_FOLDER = os.getenv("IMAGE_FOLDER")
 PDFS_FOLDER = os.getenv("PDFS_FOLDER")
 VIDEOS_FOLDER = os.getenv("VIDEOS_FOLDER")
 
-# mover para as pastas equivalentes ao tipo de arquivo
+
+def mover_arquivos():
+    # mover para as pastas equivalentes ao tipo de arquivo
+
+    for arquivo in os.listdir(MIDIA_FOLDER):
+        if arquivo.endswith(".png"):
+            caminho_origem = os.path.join(MIDIA_FOLDER, arquivo)
+            caminho_destino = os.path.join(IMAGE_FOLDER, arquivo)
+            shutil.move(caminho_origem, caminho_destino)
+        # elif arquivo.endswith(".png"):
+        #     shutil.move(arquivo, IMAGE_FOLDER)
+        # elif arquivo.endswith(".mp4"):
+        #     shutil.move(arquivo, VIDEOS_FOLDER)
+        else:
+            print(f"Arquivo não classificado: {arquivo}")
+
+if __name__ == "__main__":
+    mover_arquivos()
