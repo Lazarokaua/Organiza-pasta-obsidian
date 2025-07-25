4c335c7a83dc8cb4da862706af516333c89215d8
Lazarokaua <lazarokaua4@gmail.com>
2025-07-25 14:15:22 -0300

    feat: adicionado rename_files.py para renomear meus pdfs da faculdade

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import glob
 import shutil
 import os
 
+from rename_files import renomear_arquivos
 from dotenv import load_dotenv
 # listar a pasta que vou observar
 load_dotenv()
@@ -13,20 +14,36 @@ PDFS_FOLDER = os.getenv("PDFS_FOLDER")
 VIDEOS_FOLDER = os.getenv("VIDEOS_FOLDER")
 
 
-def mover_arquivos():
+if __name__ == "__main__":
     # mover para as pastas equivalentes ao tipo de arquivo
 
+    # tuples
+    tuple_pastas = (AUDIO_FOLDER, IMAGE_FOLDER, PDFS_FOLDER, VIDEOS_FOLDER)
+    tuple_audios = (".m4a", ".flac", ".mp3", ".wav", ".wma")
+    tuple_images = (".jpeg", ".jpg", ".png", ".gif", ".tiff")
+    tuple_videos = (".mp4", ".mov", ".avi", ".mkv", ".flv")
+    tuple_pdfs = (".pdf",)
+
     for arquivo in os.listdir(MIDIA_FOLDER):
-        if arquivo.endswith(".png"):
-            caminho_origem = os.path.join(MIDIA_FOLDER, arquivo)
+        caminho_origem = os.path.join(MIDIA_FOLDER, arquivo)
+
+        # Primeiro, verifica se é um diretório para ignorá-lo
+        if os.path.isdir(caminho_origem):
+            print(f"Ignorando pasta: {arquivo}")
+            continue
+
+        if arquivo.endswith(tuple_images):
             caminho_destino = os.path.join(IMAGE_FOLDER, arquivo)
             shutil.move(caminho_origem, caminho_destino)
-        # elif arquivo.endswith(".png"):
-        #     shutil.move(arquivo, IMAGE_FOLDER)
-        # elif arquivo.endswith(".mp4"):
-        #     shutil.move(arquivo, VIDEOS_FOLDER)
+        elif arquivo.endswith(tuple_pdfs):
+            caminho_destino = os.path.join(PDFS_FOLDER, arquivo)
+            shutil.move(caminho_origem, caminho_destino)
+        elif arquivo.endswith(tuple_videos):
+            caminho_destino = os.path.join(VIDEOS_FOLDER, arquivo)
+            shutil.move(caminho_origem, caminho_destino)
+        elif arquivo.endswith(tuple_audios):
+            caminho_destino = os.path.join(AUDIO_FOLDER, arquivo)
+            shutil.move(caminho_origem, caminho_destino)
         else:
             print(f"Arquivo não classificado: {arquivo}")
 
-if __name__ == "__main__":
-    mover_arquivos()
---
+++ rename_files.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+MIDIA_FOLDER = os.getenv("MIDIA_FOLDER")
+
+def renomear_arquivos(arquivos, materia):
+    count = 1
+    for arquivo in os.listdir(arquivos):
+        caminho_origem = os.path.join(arquivos, arquivo)
+
+        if os.path.isdir(caminho_origem):
+            print(f"{arquivo} Pasta encontrda, ignorando...")
+            continue
+
+        materia_name = f"{materia}_unidade_{count}.pdf"
+        caminho_destino = os.path.join(arquivos, materia_name)
+        os.rename(caminho_origem, caminho_destino)
+        print(arquivo)
+        count += 1
+
+
+if __name__ == "__main__":
+    # renomear_arquivos(MIDIA_FOLDER, "ESG")
+    # renomear_arquivos(MIDIA_FOLDER, "IA")
+    # renomear_arquivos(MIDIA_FOLDER, "Metodos_ageis")
+    # renomear_arquivos(MIDIA_FOLDER, "Negócios_inovadores")
+    renomear_arquivos(MIDIA_FOLDER, "mobile")
